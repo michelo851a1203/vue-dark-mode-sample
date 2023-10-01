660ba135a0649ded1d1ccf4a691c6844fde73115
MichaelHo <michelo851a1203@gmail.com>
2023-10-01 13:51:21 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import { useToggle, useDark , useColorMode } from '@vueuse/core'
+  const colorMode = useColorMode();
+  const isDark = useDark();
+  const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+  {{ colorMode }}
+  <button @click="toggleDark()">ToDarkOrLight</button>
+  <div w-32 h-32 bg="blue-400 dark:red-600" rounded-full></div>
+</template>
+
+<style>
+.dark {
+  background-color: #4b5563;
+}
+</style>
